1633a1ddbc17c8b190f6b36b7459afcbca158fd6
anas-443005129 <113012744+anas-443005129@users.noreply.github.com>
2025-09-08 15:55:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,10 @@ app = Flask(__name__)
 
 @app.get("/")
 def home():
-    return "🎉 Hello from Flask on Azure App Service!"
+    return "Hello from Flask on Azure App Service!  Anas Alzahrani"
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8000))
     # Bind to 0.0.0.0 so Azure can reach it
     app.run(host="0.0.0.0", port=port)
+
